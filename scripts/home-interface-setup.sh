ad59f0d9f67c577c48b9d46bd6749ebc679d62e4
Z Alos <satya123m4@gmail.com>
2025-04-29 13:22:48 +0530

    fix:: firefox --new-window -> removed

--- scripts/home-interface-setup.sh
+++ scripts/home-interface-setup.sh
@@ -12,7 +12,7 @@ tmux new-window -t "$SESSION" -n "Friday"
 tmux send-keys -t "$SESSION:1" "cd ~/Documents/FRIDAY/ && source fridayEnv/bin/activate && python ./friday.py" C-m
 
 tmux new-window -t "$SESSION" -n "Audio Visualizer" 
-tmux send-keys -t "$SESSION:2" "firefox --new-window https://friday-av.web.app" C-m
+tmux send-keys -t "$SESSION:2" "firefox https://friday-av.web.app" C-m
 
 tmux attach-session -t "$SESSION"
 
