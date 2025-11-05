0b3b6c13655f6fb6aefa802b736c4f436b81a9ff
Zalos <satya123m4@gmail.com>
2025-11-05 23:43:47 +0530

    fix: setup_linux.sh

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -3,18 +3,24 @@
 
 set -e  # Exit script if any command fails
 
+# [Note] Prerequisite:- 
+# Install all the packages
+# Setup Git 
+
 # First setup the WM config
+xdg-user-dirs-update && \
+cd ~/Documents && \
+cd Arch-Configuration && \
 cp -r config/* ~/.config
+
+# Setup Zsh Config
+cp -r ~/Documents/Arch-Configuration/zsh/.* ~ 
 
 # Keyboard Setup
 cd "Keyboard Layouts" 
-cp .Xmodmap ~/.Xmodmap 
+cp Xmodmap_Dvorak.config ~/.Xmodmap 
 cp change_to_dvorak.sh ~ 
 bash ~/change_to_dvorak.sh 
-
-# Setup Zsh Config
-cd .. 
-cp -r zsh/* ~ 
 
 # Neovim Setup
 mkdir -p ~/.config/nvim 
@@ -22,4 +28,3 @@ cd ~/Documents
 git clone git@github.com:Z-Alos/Neovim-Config.git 
 cp -r Neovim-Config/* ~/.config/nvim 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
